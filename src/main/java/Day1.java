68d88733ac31be26f69f7ea6506fde76cc8f5141
Henrik Ygge <henrik@meepo.se>
2024-11-30 10:53:53 +0100

    setup of new project

---
+++ src/main/java/Day1.java
@@ -0,0 +1,15 @@
+import util.Util;
+
+import java.util.List;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        var input = Util.readInts();
+        Util.submitPart1(part1(input));
+    }
+
+    private static int part1(List<Integer> input) {
+        return 0;
+    }
+}
